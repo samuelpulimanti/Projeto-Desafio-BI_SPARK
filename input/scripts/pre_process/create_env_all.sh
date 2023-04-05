ee632e0fd5cb1f810109afafdcd751e37bb565b4
Samuel Pulimanti <dantepulimanti@gmail.com>
2023-04-05 14:58:35 +0000

    Script criado

--- input/scripts/pre_process/create_env_all.sh
+++ input/scripts/pre_process/create_env_all.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# Criação das pastas
+# Criação das pastasssss
 
-DADOS=("cidade" "estado" "filial" "parceiro" "cliente" "subcategoria" "categoria" "item_pedido" "produto")
+DADOS=("CLIENTES" "DIVISAO" "ENDERECO" "REGIAO" "VENDAS")
 
 for i in "${DADOS[@]}"
 do
-	echo "$i"
-    cd ../../raw/
+	echo "Criando as pastas $i"
+    mkdir ../../raw/$i
+    chmod 777 ../../raw/$i
+    cd ../../raw/ 
+    curl -O https://raw.githubusercontent.com/samuelpulimanti/Projeto-Desafio-BI_SPARK/main/input/raw/$i.csv  
     hdfs dfs -mkdir /datalake/raw/$i
     hdfs dfs -chmod 777 /datalake/raw/$i
     hdfs dfs -copyFromLocal $i.csv /datalake/raw/$i
-    beeline -u jdbc:hive2://localhost:10000 -f ../../scripts/hql/create_table_$i.hql 
+    beeline -u jdbc:hive2://localhost:10000/DESAFIO_CURSO -f ../../scripts/hql/create_table_$i.hql
+
 done
