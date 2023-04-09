7454b6acf14e1f925590c3c6f281658bb0ea9535
Samuel Pulimanti <dantepulimanti@gmail.com>
2023-04-09 02:54:07 +0000

    Fim

--- input/scripts/pre_process/create_env_all.sh
+++ input/scripts/pre_process/create_env_all.sh
@@ -5,10 +5,10 @@ DADOS=("CLIENTES" "DIVISAO" "ENDERECO" "REGIAO" "VENDAS")
 for i in "${DADOS[@]}"
 do
 	echo "$i"
-    #cd ../../raw/
-	#hdfs dfs -mkdir /datalake/raw/$i
-    #hdfs dfs -chmod 777 /datalake/raw/$i
-    #hdfs dfs -copyFromLocal $i.csv /datalake/raw/$i
-    beeline -u jdbc:hive2://localhost:10000/desafio_curso -f ../../scripts/hql/create_table_$i.hql
+    cd ../../raw/
+	hdfs dfs -mkdir /datalake/raw/$i
+    hdfs dfs -chmod 777 /datalake/raw/$i
+    hdfs dfs -copyFromLocal $i.csv /datalake/raw/$i
+    #beeline -u jdbc:hive2://localhost:10000/desafio_curso -f ../../scripts/hql/create_table_$i.hql
 
 done
